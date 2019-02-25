66d9aef90a547451b5a72b006d357c0704edb2c2
Daniel CHIN Wen Kai <daniel.chin.2017@business.smu.edu.sg>
2019-02-25 15:18:01 +0800

    Skeleton Template

---
+++ main/ui.R
@@ -0,0 +1,61 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(shinydashboard)
+
+# Define UI for application that draws a histogram
+shinyUI(
+  ui <- dashboardPage(
+    dashboardHeader(title = "EzModel"),
+    dashboardSidebar(
+      sidebarMenu(
+        menuItem("Overview", tabName = "overview", icon = icon("dashboard")),
+        menuItem("GWR Model", tabName = "gwrmodel", icon = icon("th"))
+      )
+    ),
+    dashboardBody(
+      tabItems(
+        # First tab content
+        tabItem(tabName = "overview",
+                fluidPage(
+                  sidebarLayout(
+                    sidebarPanel(
+                      selectInput(inputId = "year", 
+                                  label = "Year:",
+                                  choices = c("2013", "2014"), 
+                                  selected = "2013")
+                    ),
+                    mainPanel(
+                      
+                    )
+                  )
+                )
+        ),
+        
+        # Second tab content
+        tabItem(tabName = "gwrmodel",
+          # Boxes need to be put in a row (or column)
+          fluidPage(
+            sidebarLayout(
+              sidebarPanel(
+                selectInput(inputId = "test", 
+                            label = "Test",
+                            choices = c("A", "B"), 
+                            selected = "A")
+              ),
+              mainPanel(
+                
+              )
+            )
+          )  
+        )
+      )
+    )
+  ))
