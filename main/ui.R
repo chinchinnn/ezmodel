7db713852acb1b9d9c423b4d8e47b81ae636b794
plyh <patricklyh95@gmail.com>
2019-03-20 22:14:19 +0800

    Update ui.R

--- main/ui.R
+++ main/ui.R
@@ -127,12 +127,12 @@ shinyUI(
                                       )
                              )
                            )
-                           # sidebarLayout(
-                           #   sidebarPanel(sliderInput("year", "Year:", min = 1968, max = 2009, value = 2009, sep='')),
-                           #   mainPanel(
-                           #     ##
-                           #   )
-                           # )
+                           sidebarLayout(
+                             sidebarPanel(sliderInput("year", "Year:", min = 1968, max = 2009, value = 2009, sep='')),
+                             mainPanel(
+                               ##
+                             )
+                           )
                   ),
                   tabPanel("View Data", fluid = TRUE,
                           fluidPage(
